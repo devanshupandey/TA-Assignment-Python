b4da9c497b08e4a7345e37f481b79a10fbad5dcf
devanshupandey <79018712+devanshupandey@users.noreply.github.com>
2021-03-12 21:42:21 +0530

    Add files via upload

---
+++ grocery.py
@@ -0,0 +1,27 @@
+grocery_item = { } 
+grocery_history = [ ]
+b = input('enter your budget')
+stop = False
+grand_total = 0
+while not stop:
+        item_name = input("Enter Product :\n")
+        quantity = input("Enter Quantity :\n")
+        cost = input("Enter Price :\n")
+        grocery_item = {'Product name':item_name,'Quantity':int(quantity),'Price':int(cost)}
+        
+        for index, item in enumerate(grocery_history):
+             item_total = grocery_item['Quantity']*grocery_item['Price']
+             grand_total = grand_total + item_total
+             if (grand_total > budget):
+                 print("item cannot be bought")
+             else:
+                     grocery_history.append(grocery_item)
+        user_input = input("Enter your choice")
+        if user_input == '2':
+            stop = True
+        else:
+            stop = False
+
+
+print(*grocery_history, sep ='\n')
+      
